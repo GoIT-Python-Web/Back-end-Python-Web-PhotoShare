8b6a03c72142fd9ee538b21c051450e6119a30c3
vlad hetman <vladislavgetman704@gmail.com>
2025-04-02 17:23:56 +0200

    JWT

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -2,14 +2,17 @@ from sqlalchemy.orm import Session
 from src.entity.models import User
 from src.schemas.user_schema import UserCreate
 from src.core.security import get_password_hash
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
 
-def create_user(db: Session, user: UserCreate, is_admin: bool = False):
-    hashed_password = get_password_hash(user.password)
-    db_user = User(email=user.email, hashed_password=hashed_password, is_admin=is_admin)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+async def get_user_by_email(db: AsyncSession, email: str):
+    result = await db.execute(select(User).filter(User.email == email))
+    return result.scalars().first()
+
+async def create_user(db: AsyncSession, user_data):
+    new_user = User(**user_data)
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+    return new_user
--- src/schemas/user_schema.py
+++ src/schemas/user_schema.py
@@ -1,9 +1,34 @@
 from pydantic import BaseModel, EmailStr
-
-class UserCreate(BaseModel):
-    email: EmailStr
-    password: str
+from datetime import date
+from typing import Optional
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    type: str  # Наприклад: "admin" або "user"
+    name: Optional[str] = None
+    phone: Optional[str] = None
+    birthdate: Optional[date] = None
+    description: Optional[str] = None
+    img_link: Optional[str] = None
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    type: str
+    name: Optional[str]
+    phone: Optional[str]
+    birthdate: Optional[date]
+    description: Optional[str]
+    img_link: Optional[str]
+    is_banned: bool
+
+    class Config:
+        from_attributes = True
+
