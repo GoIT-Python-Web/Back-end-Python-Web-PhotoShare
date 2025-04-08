c6c60ef28aa12e660a018a61d8eaf7866aa4a86c
Dima <naymenko.dmitry5@gmail.com>
2025-04-08 21:44:10 +0300

    user returned

--- src/schemas/comment.py
+++ src/schemas/comment.py
@@ -1,12 +1,32 @@
 from pydantic import BaseModel
 from uuid import UUID
 from datetime import datetime
+from enum import Enum
+
+
+class UserTypeEnum(str, Enum):
+    user = "user"
+    admin = "admin"
+
+
+class UserShortDTO(BaseModel):
+    id: UUID
+    name: str | None
+    img_link: str | None
+    type: UserTypeEnum
+
+    model_config = {
+        "from_attributes": True
+    }
+
 
 class CommentCreateDTO(BaseModel):
     message: str
 
+
 class CommentUpdateDTO(BaseModel):
     message: str
+
 
 class CommentOut(BaseModel):
     id: UUID
@@ -16,8 +36,8 @@ class CommentOut(BaseModel):
     is_deleted: bool
     created_at: datetime
     updated_at: datetime
+    user: UserShortDTO
 
     model_config = {
         "from_attributes": True 
     }
-
